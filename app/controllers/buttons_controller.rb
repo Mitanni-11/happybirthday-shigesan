676d8ab947de3aa16d2077acee7759bb073a6768
Hironori Mitanie <sangudadian@gmail.com>
2020-04-16 06:21:07 +0900

    fix routing/client

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -12,11 +12,11 @@ class ButtonsController < ApplicationController
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
+    unless @client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
 
-    events = client.parse_events_from(body)
+    events = @client.parse_events_from(body)
     events.each do |event|
       case event
       when Line::Bot::Event::Message
@@ -26,9 +26,9 @@ class ButtonsController < ApplicationController
               type: 'text',
               text: event.message['メッセージを受け取ったよ！ ありがとう！！']
           }
-          client.reply_message(event['replyToken'], message)
+          @client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
+          response = @client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
           tf.write(response.body)
         end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'buttons#share'
   get 'buttons/share'
   get 'buttons/first'
-  post 'line_callback' => 'buttons/line_message'
+  post 'line_callback' => 'buttons#line_message'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
