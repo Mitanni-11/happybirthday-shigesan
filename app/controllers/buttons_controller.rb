5cca3e3a0742af3061fea0f7d7fb5908e663ab1e
Hironori Mitanie <sangudadian@gmail.com>
2020-04-16 06:15:24 +0900

    test line message api

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -1,7 +1,40 @@
 class ButtonsController < ApplicationController
-  def share
-  end
+  require 'line/bot'
 
-  def first
+  def line_message
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+
+
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+              type: 'text',
+              text: event.message['メッセージを受け取ったよ！ ありがとう！！']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+
+    end
+
   end
 end
